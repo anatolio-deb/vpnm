b7d5382989f6394f2da3ee9976703818d57b9056
Anatolio Nikiforidis <nikiforova693@gmail.com>
2021-06-10 22:46:52 +0500

    refactor: install script

--- install.py
+++ install.py
@@ -6,17 +6,21 @@ LINKS = {
     "https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh": "install-release.sh",
     "https://github.com/iochen/v2gen/releases/download/v2.0.1/v2gen_amd64_linux": "v2gen_amd64_linux",
     "https://github.com/xjasonlyu/tun2socks/releases/download/v2.2.0/tun2socks-linux-amd64.zip": "tun2socks-linux-amd64.zip",
-    "https://github.com/cloudflare/cloudflared/releases/download/2021.5.10/cloudflared-linux-amd64": "cloudflared-linux-amd64",
+    "https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb": "cloudflared-stable-linux-amd64.deb",
     "https://github.com/anatolio-deb/vpnmd/releases/download/latest/vpnmd": "vpnmd",
     "https://github.com/anatolio-deb/vpnmd/blob/main/vpnmd.service": "vpnmd.service",
     "https://github.com/anatolio-deb/vpnm/releases/download/latest/vpnm": "vpnm",
 }
 BIN_PATH = "/usr/local/bin"
-DPATH = "/var/local"
+DPATH = "/var/local/bin"
 SVC_PATH = "/etc/systemd/system"
 
 for link, filename in LINKS.items():
-    if filename in ["install-release.sh", "tun2socks-linux-amd64.zip"]:
+    if filename in [
+        "install-release.sh",
+        "tun2socks-linux-amd64.zip",
+        "cloudflared-stable-linux-amd64.deb",
+    ]:
         filepath = f"/tmp/{filename}"
 
         if filename == "tun2socks-linux-amd64.zip":
@@ -24,6 +28,14 @@ for link, filename in LINKS.items():
             def post_dl_act():
                 with zipfile.ZipFile("/tmp/tun2socks-linux-amd64.zip", "r") as zip_ref:
                     zip_ref.extractall(BIN_PATH)
+
+        elif filename == "cloudflared-stable-linux-amd64.deb":
+
+            def post_dl_act():
+                subprocess.run(
+                    ["dpkg", "-i", "/tmp/cloudflared-stable-linux-amd64.deb"],
+                    check=True,
+                )
 
         else:
 
@@ -51,9 +63,14 @@ for link, filename in LINKS.items():
         print(f"Extracting {filepath} to {BIN_PATH}")
     else:
         print(f"Downloaded {filepath}")
-    if filename in ["install-release.sh", "tun2socks-linux-amd64.zip"]:
+
+    if filename in [
+        "install-release.sh",
+        "tun2socks-linux-amd64.zip",
+        "cloudflared-stable-linux-amd64.deb",
+    ]:
         post_dl_act()
 
 subprocess.run(["systemctl", "daemon-reload"], check=True)
-subprocess.run(["systemctl", "restart", "vpnmd.service"], check=True)
+subprocess.run(["systemctl", "enable", "--now", "vpnmd"], check=True)
 subprocess.run(["vpnm"], check=True)
