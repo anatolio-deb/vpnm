6e5ff4fc7403739cb20398387c952b51e83eb066
Anatolio Nikiforidis <nikiforova693@gmail.com>
2021-07-02 09:27:22 +0500

    ci: installation script

--- install.py
+++ install.py
@@ -2,59 +2,59 @@ import subprocess
 import zipfile
 from urllib.request import Request, urlopen
 
-LINKS = {
-    "https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh": "install-release.sh",
-    "https://github.com/iochen/v2gen/releases/download/v2.0.1/v2gen_amd64_linux": "v2gen_amd64_linux",
-    "https://github.com/xjasonlyu/tun2socks/releases/download/v2.2.0/tun2socks-linux-amd64.zip": "tun2socks-linux-amd64.zip",
-    "https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb": "cloudflared-stable-linux-amd64.deb",
-    "https://github.com/anatolio-deb/vpnmd/releases/download/latest/vpnmd": "vpnmd",
-    "https://github.com/anatolio-deb/vpnmd/blob/main/vpnmd.service": "vpnmd.service",
-    "https://github.com/anatolio-deb/vpnm/releases/download/latest/vpnm": "vpnm",
-}
-BIN_PATH = "/usr/local/bin"
-DPATH = "/var/local/bin"
-SVC_PATH = "/etc/systemd/system"
-POST_DL = [
-    "install-release.sh",
-    "tun2socks-linux-amd64.zip",
-    "cloudflared-stable-linux-amd64.deb",
-    "v2gen_amd64_linux",
+LINKS = [
+    "https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh",
+    "https://github.com/xjasonlyu/tun2socks/releases/download/v2.2.0/tun2socks-linux-amd64.zip",
+    "https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb",
+    "https://github.com/iochen/v2gen/releases/download/v2.0.1/v2gen_amd64_linux",
+    "https://github.com/anatolio-deb/vpnmd/releases/download/latest/vpnmd",
+    "https://github.com/anatolio-deb/vpnm/releases/download/latest/vpnm",
 ]
+BIN_PATH = "/usr/local/bin"
+SERVICE = """[Unit]
+Description=VPN Manager daemon
+After=network-online.target
+Wants=network-online.target
 
-for link, filename in LINKS.items():
-    if filename in POST_DL:
+[Service]
+Restart=on-failure
+ExecStart=/usr/local/bin/vpnmd
+
+[Install]
+WantedBy=multi-user.target"""
+
+for link in LINKS:
+    filename = link[-link[::-1].find("/") :]
+
+    if "." in filename:
         filepath = f"/tmp/{filename}"
 
-        if filename is POST_DL[0]:
+        if link is LINKS[0]:
 
             def post_dl_act():
-                subprocess.run(["bash", "/tmp/install-release.sh"], check=True)
+                subprocess.run(["bash", filepath], check=True)
 
-        elif filename is POST_DL[1]:
+        elif link is LINKS[1]:
 
             def post_dl_act():
-                with zipfile.ZipFile("/tmp/tun2socks-linux-amd64.zip", "r") as zip_ref:
+                with zipfile.ZipFile(filepath, "r") as zip_ref:
                     zip_ref.extractall(BIN_PATH)
-
-        elif filename is POST_DL[2]:
-
-            def post_dl_act():
-                subprocess.run(
-                    ["dpkg", "-i", "/tmp/cloudflared-stable-linux-amd64.deb"],
-                    check=True,
-                )
 
         else:
 
             def post_dl_act():
-                subprocess.run(["chmod", "+", "x", BIN_PATH / POST_DL[-1]], check=True)
+                subprocess.run(
+                    ["dpkg", "-i", filepath],
+                    check=True,
+                )
 
-    elif filename == "vpnmd":
-        filepath = f"{DPATH}/{filename}"
-    elif filename == "vpnmd.service":
-        filepath = f"{SVC_PATH}/{filename}"
     else:
         filepath = f"{BIN_PATH}/{filename}"
+
+        if link is LINKS[3]:
+
+            def post_dl_act():
+                subprocess.run(["chmod", "+", "x", filepath], check=True)
 
     print(f"Downloading {filename} from {link} to {filepath}")
 
@@ -64,15 +64,18 @@ for link, filename in LINKS.items():
         with open(filepath, "wb") as file:
             file.write(response.read())
 
-    if filename == "install-release.sh":
+    if filename in LINKS[0]:
         print(f"Installing v2ray from {filepath}")
-    elif filename == "tun2socks-linux-amd64.zip":
+    elif filename in LINKS[1]:
         print(f"Extracting {filepath} to {BIN_PATH}")
     else:
         print(f"Downloaded {filepath}")
 
-    if filename in POST_DL:
+    if filename in LINKS[:4]:
         post_dl_act()
+
+with open("/etc/systemd/system/vpnmd.service", "w") as file:
+    file.write(SERVICE)
 
 subprocess.run(["systemctl", "daemon-reload"], check=True)
 subprocess.run(["systemctl", "enable", "--now", "vpnmd"], check=True)
