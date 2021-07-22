45926dee1f4293c9b63abeeaa3737ac3e58aa0e2
Anatolio Nikiforidis <nikiforova693@gmail.com>
2021-07-22 20:29:33 +0500

    refactor: install script

--- install.py
+++ install.py
@@ -1,21 +1,75 @@
+import json
 import zipfile
 from http.client import IncompleteRead
+from string import Template
 from subprocess import PIPE, STDOUT, Popen, TimeoutExpired
 from threading import Thread
-from typing import Callable
+from typing import Any, Callable, Tuple
 from urllib.request import Request, urlopen
 
-LINKS = [
-    "https://github.com/v2ray/dist/raw/master/v2ray-linux-64.zip",
-    "https://github.com/xjasonlyu/tun2socks/releases/download/v2.2.0/tun2socks-linux-amd64.zip",
-    "https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-linux-amd64.deb",
-    "https://github.com/iochen/v2gen/releases/download/v2.0.1/v2gen_amd64_linux",
-    "https://github.com/anatolio-deb/vpnmd/releases/download/latest/vpnmd",
-    "https://github.com/anatolio-deb/vpnm/releases/download/latest/vpnm",
-]
-BIN_PATH = "/usr/local/bin"
-UNIT_PATH = "/etc/systemd/system/vpnmd.service"
-UNIT_CONTENT = """[Unit]
+
+class GitHubAPI:
+    def __init__(self) -> None:
+        self.browser_download_urls = self._get_browser_download_urls()
+
+    @staticmethod
+    def _get_api_request_urls() -> list:
+        api_request_template = Template(
+            "https://api.github.com/repos/$user/$repository/releases/latest"
+        )
+        metadata = [
+            ("xjasonlyu", "tun2socks"),
+            ("cloudflare", "cloudflared"),
+            ("iochen", "v2gen"),
+            ("anatolio-deb", "vpnmd"),
+            ("anatolio-deb", "vpnm"),
+        ]
+
+        return [
+            api_request_template.substitute(user=user, repository=repository)
+            for user, repository in metadata
+        ]
+
+    @staticmethod
+    def _get_json_response(api_request_url: str) -> dict:
+        request = Request(api_request_url, headers={"User-Agent": "Mozilla/5.0"})
+
+        with urlopen(request) as response:
+            return json.loads(response.read().decode("utf-8"))
+
+    def _get_asset(self, api_request_url: str) -> dict:
+        for asset in self._get_json_response(api_request_url)["assets"]:
+            if asset["name"] in [
+                "tun2socks-linux-amd64.zip",
+                "cloudflared-linux-amd64",
+                "v2gen_amd64_linux",
+                "vpnm",
+                "vpnmd",
+            ]:
+                return asset
+
+        raise KeyError("Asset not found")
+
+    @staticmethod
+    def _get_browser_download_url(asset: dict):
+        return asset.get("browser_download_url")
+
+    def _get_browser_download_urls(self) -> list:
+        return [
+            self._get_browser_download_url(self._get_asset(url))
+            for url in self._get_api_request_urls()
+        ]
+
+
+class Downloader:
+    github_api = GitHubAPI()
+    urls = github_api.browser_download_urls
+    urls.append("https://github.com/v2ray/dist/raw/master/v2ray-linux-64.zip")
+    bin_path = "/usr/local/bin"
+    tmp_path = "/tmp"
+    threads: list = []
+    unit_path = "/etc/systemd/system/vpnmd.service"
+    unit_content = """[Unit]
 Description=VPN Manager daemon
 After=network-online.target
 Wants=network-online.target
@@ -27,130 +81,111 @@ ExecStart=/usr/local/bin/vpnmd
 [Install]
 WantedBy=multi-user.target"""
 
+    @staticmethod
+    def chmod(filepath: str):
+        with Popen(["chmod", "+x", filepath], stdout=PIPE, stderr=STDOUT) as process:
+            stdout = process.communicate()[0]
 
-def get_filename_from_link(link: str) -> str:
-    return link[-link[::-1].find("/") :]
+            if process.returncode != 0:
+                raise RuntimeError(stdout)
 
+    def unzip(self, filepath: str):
+        with zipfile.ZipFile(filepath, "r") as zip_ref:
+            zip_ref.extractall(self.bin_path)
 
-def get_filepath_from_filename(filename: str) -> str:
-    if "." in filename:
-        return f"/tmp/{filename}"
-    return f"{BIN_PATH}/{filename}"
+    @staticmethod
+    def get_filename_from_url(url: str) -> str:
+        return url[-url[::-1].find("/") :]
 
+    def get_filepath_from_filename(
+        self, filename: str
+    ) -> Tuple[str, Callable[[str], Any]]:
+        if ".zip" in filename:
+            return (f"{self.tmp_path}/{filename}", self.unzip)
 
-def get_post_download_action(filename: str, filepath: str) -> Callable:
-    if ".zip" in filename:
+        return (f"{self.bin_path}/{filename}", self.chmod)
 
-        def post_download_action():
-            with zipfile.ZipFile(filepath, "r") as zip_ref:
-                zip_ref.extractall(BIN_PATH)
+    @staticmethod
+    def download(request: Request, filepath: str, callback=None):
 
-    elif ".deb" in filename:
+        with urlopen(request) as response:
+            with open(filepath, "wb") as file:
+                file.write(response.read())
 
-        def post_download_action():
-            with Popen(["dpkg", "-i", filepath], stdout=PIPE, stderr=STDOUT) as process:
-                stdout = process.communicate()[0]
+        if callback is not None:
+            callback(filepath)
 
-                if process.returncode != 0:
-                    raise RuntimeError(stdout)
+    def process_urls(self):
+        for url in self.urls:
+            filename = self.get_filename_from_url(url)
+            filepath, callback = self.get_filepath_from_filename(filename)
+            request = Request(url, headers={"User-Agent": "Mozilla/5.0"})
+            thread = Thread(
+                target=self.download,
+                args=(
+                    request,
+                    filepath,
+                    callback,
+                ),
+            )
+            thread.start()
+            self.threads.append(thread)
 
-    else:
+        for thread in self.threads:
+            thread.join()
 
-        def post_download_action():
-            with Popen(
-                ["chmod", "+x", filepath], stdout=PIPE, stderr=STDOUT
-            ) as process:
-                stdout = process.communicate()[0]
+        self.post_process()
 
-                if process.returncode != 0:
-                    raise RuntimeError(stdout)
+    def post_process(self):
+        with open(self.unit_path, "w") as file:
+            file.write(self.unit_content)
 
-    return post_download_action
+        with Popen(
+            ["systemctl", "daemon-reload"], stdout=PIPE, stderr=STDOUT
+        ) as process:
+            stdout = process.communicate()[0]
 
+            if process.returncode != 0:
+                raise RuntimeError(stdout)
 
-def download(link: str, filepath: str) -> None:
-    request = Request(link, headers={"User-Agent": "Mozilla/5.0"})
+        with Popen(
+            ["systemctl", "enable", "--now", "vpnmd"], stdout=PIPE, stderr=STDOUT
+        ) as process:
+            stdout = process.communicate()[0]
 
-    with urlopen(request) as response:
-        with open(filepath, "wb") as file:
-            file.write(response.read())
+            if process.returncode != 0:
+                raise RuntimeError(stdout)
 
-
-def post_process():
-    with open(UNIT_PATH, "w") as file:
-        file.write(UNIT_CONTENT)
-
-    with Popen(["systemctl", "daemon-reload"], stdout=PIPE, stderr=STDOUT) as process:
-        stdout = process.communicate()[0]
-
-        if process.returncode != 0:
-            raise RuntimeError(stdout)
-
-    with Popen(
-        ["systemctl", "enable", "--now", "vpnmd"], stdout=PIPE, stderr=STDOUT
-    ) as process:
-        stdout = process.communicate()[0]
-
-        if process.returncode != 0:
-            raise RuntimeError(stdout)
-
-
-def downloader(link):
-    filename = get_filename_from_link(link)
-    filepath = get_filepath_from_filename(filename)
-    post_download_action = get_post_download_action(filename, filepath)
-    download(link, filepath)
-
-    if ".zip" not in filename:
-        post_download_action()
-
-
-def process_links(testing: bool):
-    if testing:
-        links = LINKS[1:]
-    else:
-        links = LINKS
-
-    threads = []
-
-    for link in links:
-        _thread = Thread(target=downloader, args=(link,))
-        threads.append(_thread)
-        _thread.start()
-
-    for thread in threads:
-        thread.join()
-
-
-def main(testing=False):
-    print(
-        f"""Welcome to VPN Manager!
+    def runner(self):
+        print(
+            f"""Welcome to VPN Manager!
 
 This will download and install the latest version of vpnm,
 an alternative CLI client for VPN Manager.
 
 It will add the `vpnm` command to system's bin directory, located at:
 
-{BIN_PATH}
+{self.bin_path}
 
 You can uninstall at any time by executing this script with the --uninstall option,
 and these changes will be reverted."""
-    )
+        )
 
-    try:
-        process_links(testing)
-        post_process()
-    except (ConnectionError, RuntimeError, TimeoutExpired, IncompleteRead) as ex:
-        print(ex)
-    else:
-        print(
-            """VPN Manager is installed now. Great!
+        try:
+            self.process_urls()
+            self.post_process()
+        except (ConnectionError, RuntimeError, TimeoutExpired, IncompleteRead) as ex:
+            print(ex)
+        else:
+            print(
+                """VPN Manager is installed now. Great!
 
 You can test that everything is set up by executing:
 
 `vpnm --help`"""
-        )
+            )
 
 
 if __name__ == "__main__":
-    main()
+    downloader = Downloader()
+    downloader.runner()
