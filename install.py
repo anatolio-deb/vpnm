5196eabfe9c6b2aaa0d20431caa79b356c7000e4
Anatolio Nikiforidis <nikiforova693@gmail.com>
2021-07-25 19:13:42 +0500

    ci: callbacks added

--- install.py
+++ install.py
@@ -66,7 +66,7 @@ class Downloader:
     github_api = GitHubAPI()
     urls = github_api.browser_download_urls
     urls.append("https://github.com/v2ray/dist/raw/master/v2ray-linux-64.zip")
-    bin_path = Path("/usr/local/bin/vpnm/")
+    bin_path = Path("/usr/local/bin/")
     tmp_path = "/tmp"
     threads: list = []
 
@@ -75,13 +75,36 @@ class Downloader:
         return url[-url[::-1].find("/") :]
 
     def get_filepath_and_callback(self, filename: str) -> Tuple:
-        def unzip(filepath: str):
+        def unzip(filepath: str, target: str = None):
+            """Extract the whole zip archive or an exact file from it.
+
+            Args:
+                filepath (str): The location of a zip file
+                target (str, optional): The exact file to extract. Defaults to None.
+            """
             with zipfile.ZipFile(filepath, "r") as zip_ref:
-                zip_ref.extractall(self.bin_path)
+                if target is not None:
+                    for member in zip_ref.infolist():
+                        if member.filename == target:
+                            zip_ref.extract(member, self.bin_path)
+
+                    if not Path(f"{self.bin_path}/{target}").exists():
+                        raise FileNotFoundError(f"{target} is not found in {filepath}")
+                else:
+                    zip_ref.extractall(self.bin_path)
+
+        def chmod(filepath: str):
+            with Popen(
+                ["chmod", "ugo+x", filepath], stdout=PIPE, stderr=STDOUT
+            ) as process:
+                stdout = process.communicate()[0]
+
+                if process.returncode != 0:
+                    raise RuntimeError(stdout)
 
         if self.tmp_path and ".zip" in filename:
             return (f"{self.tmp_path}/{filename}", unzip)
-        return (f"{self.bin_path}/{filename}", None)
+        return (f"{self.bin_path}/{filename}", chmod)
 
     @staticmethod
     def download(request: Request, filepath: str, callback=None):
@@ -91,7 +114,10 @@ class Downloader:
                 file.write(response.read())
 
         if callback is not None:
-            callback(filepath)
+            if "v2ray" in filepath:
+                callback(filepath, "v2ray")
+            else:
+                callback(filepath)
 
     def process_urls(self):
         for url in self.urls:
@@ -132,7 +158,7 @@ Wants=network-online.target
 
 [Service]
 Restart=on-failure
-ExecStart=/usr/local/bin/vpnm/vpnmd
+ExecStart=/usr/local/bin/vpnmd
 
 [Install]
 WantedBy=multi-user.target"""
@@ -157,14 +183,6 @@ WantedBy=multi-user.target"""
             if process.returncode != 0:
                 raise RuntimeError(stdout)
 
-        with Popen(
-            ["chmod", "-R", "ugo+rx", Downloader.bin_path], stdout=PIPE, stderr=STDOUT
-        ) as process:
-            stdout = process.communicate()[0]
-
-            if process.returncode != 0:
-                raise RuntimeError(stdout)
-
     def uninstall(self):
         with Popen(
             ["systemctl", "disable", "--now", "vpnmd"], stdout=PIPE, stderr=STDOUT
@@ -174,19 +192,25 @@ WantedBy=multi-user.target"""
             if process.returncode != 0:
                 raise RuntimeError(stdout)
 
-        with Popen(["rm", self.unit_path], stdout=PIPE, stderr=STDOUT) as process:
-            stdout = process.communicate()[0]
+        filenames = [
+            "cloudflared-linux-amd64",
+            "tun2socks-linux-amd64",
+            "v2gen_amd64_linux",
+            "v2ray",
+            "vpnm",
+            "vpnmd",
+        ]
+        paths = [Path(f"{Downloader.bin_path}/{filename}") for filename in filenames]
 
-            if process.returncode != 0:
-                raise RuntimeError(stdout)
+        paths.extend(self.unit_path)
 
-        with Popen(
-            ["rm", "-r", "-f", Downloader.bin_path], stdout=PIPE, stderr=STDOUT
-        ) as process:
-            stdout = process.communicate()[0]
+        for path in paths:
+            if path.exists() and path.is_file():
+                with Popen(["rm", path], stdout=PIPE, stderr=STDOUT) as process:
+                    stdout = process.communicate()[0]
 
-            if process.returncode != 0:
-                raise RuntimeError(stdout)
+                    if process.returncode != 0:
+                        raise RuntimeError(stdout)
 
 
 if __name__ == "__main__":
