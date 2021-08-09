b9372aba9bd9233481e475a2a7c7270fe4840f32
Anatolio Nikiforidis <nikiforova693@gmail.com>
2021-08-09 08:17:50 +0500

    refactor: ci no colorama, linted

--- install.py
+++ install.py
@@ -5,21 +5,20 @@ from pathlib import Path
 from subprocess import PIPE, STDOUT, Popen, SubprocessError
 from threading import Thread
 from urllib.request import Request, urlopen
-from colorama import init, Fore
 
 
 class GitHubAPI:
     filenames = [
-                "tun2socks-linux-amd64.zip",
-                "cloudflared-linux-amd64",
-                "v2gen_amd64_linux",
-                "vpnm",
-                "vpnmd",
-            ]
-    urls = {}
+        "tun2socks-linux-amd64.zip",
+        "cloudflared-linux-amd64",
+        "v2gen_amd64_linux",
+        "vpnm",
+        "vpnmd",
+    ]
+    urls: dict = {}
 
     def __init__(self) -> None:
-        self.browser_download_urls = self._get_browser_download_urls()
+        self._set_browser_download_urls()
 
     @staticmethod
     def _get_api_request_urls() -> list:
@@ -55,7 +54,7 @@ class GitHubAPI:
     def _get_browser_download_url(asset: dict):
         return asset.get("browser_download_url")
 
-    def _get_browser_download_urls(self) -> list:
+    def _set_browser_download_urls(self):
         for url in self._get_api_request_urls():
             asset = self._get_asset(url)
             self.urls[asset["name"]] = self._get_browser_download_url(asset)
@@ -63,11 +62,13 @@ class GitHubAPI:
 
 class Downloader:
     github_api = GitHubAPI()
-    github_api.urls["v2ray-linux-64.zip"] = "https://github.com/v2ray/dist/raw/master/v2ray-linux-64.zip"
+    github_api.urls[
+        "v2ray-linux-64.zip"
+    ] = "https://github.com/v2ray/dist/raw/master/v2ray-linux-64.zip"
     bin_path = Path("/usr/local/bin")
     tmp_path = Path("/tmp")
     threads: list = []
-    filenames = ["tun2socks-linux-amd64", "v2ray", 'geoip.dat', 'geosite.dat']
+    filenames = ["tun2socks-linux-amd64", "v2ray", "geoip.dat", "geosite.dat"]
 
     @staticmethod
     def run(command: list):
@@ -79,8 +80,7 @@ class Downloader:
                 raise SubprocessError(stdout.decode())
 
     def download(self, request: Request, filename: str):
-
-        def unzip(filepath: str, target: str):
+        def unzip(filepath: Path, target: str):
             """Extract the whole zip archive or an exact file from it.
 
             Args:
@@ -90,27 +90,30 @@ class Downloader:
 
             with zipfile.ZipFile(filepath, "r") as zip_ref:
                 for member in zip_ref.infolist():
-                    if member.filename == target and not (
-                        self.bin_path / member.filename
-                        ).exists():
+                    if (
+                        member.filename == target
+                        and not (self.bin_path / member.filename).exists()
+                    ):
                         zip_ref.extract(member, self.bin_path)
 
         with urlopen(request) as response:
 
-            if 'zip' in filename:
+            if "zip" in filename:
 
                 filepath = self.tmp_path / filename
 
                 with open(filepath, "wb") as file:
                     file.write(response.read())
 
-                if 'v2ray' in filename:
-                    
+                if "v2ray" in filename:
+
                     filename = self.filenames[1]
 
-                    for member in self.filenames[1: ]:
+                    for member in self.filenames[1:]:
                         unzip(filepath, member)
-                        self.run(["chmod",  "ugo+r", (self.bin_path / member).as_posix()])
+                        self.run(
+                            ["chmod", "ugo+r", (self.bin_path / member).as_posix()]
+                        )
                 else:
                     filename = self.filenames[0]
                     unzip(filepath, filename)
@@ -118,9 +121,8 @@ class Downloader:
 
                 with open(self.bin_path / filename, "wb") as file:
                     file.write(response.read())
-            
+
             self.run(["chmod", "ugo+x", (self.bin_path / filename).as_posix()])
-            
 
     def process_urls(self):
         for filename, url in self.github_api.urls.items():
@@ -152,9 +154,15 @@ ExecStart={Downloader.bin_path}/{GitHubAPI.filenames[-1]}
 
 [Install]
 WantedBy=multi-user.target"""
-    install_commands = ["systemctl daemon-reload", f"systemctl enable --now {GitHubAPI.filenames[-1]}"]
+    install_commands = [
+        "systemctl daemon-reload",
+        f"systemctl enable --now {GitHubAPI.filenames[-1]}",
+    ]
     uninstall_commands = [f"systemctl disable --now {GitHubAPI.filenames[-1]}", "rm {}"]
-    paths = [Downloader.bin_path / filename for filename in GitHubAPI.filenames + Downloader.filenames]
+    paths = [
+        Downloader.bin_path / filename
+        for filename in GitHubAPI.filenames + Downloader.filenames
+    ]
 
     def install(self):
         with open(self.unit_path, "w") as file:
@@ -172,7 +180,7 @@ WantedBy=multi-user.target"""
                     print(ex)
             else:
                 self.paths.append(self.unit_path)
-                
+
                 for path in self.paths:
                     if path.exists() and path.is_file():
                         try:
@@ -182,7 +190,6 @@ WantedBy=multi-user.target"""
 
 
 if __name__ == "__main__":
-    init()
     parser = argparse.ArgumentParser()
     parser.add_argument("--uninstall", action="store_true", default=False)
     args = parser.parse_args()
@@ -190,34 +197,43 @@ if __name__ == "__main__":
     installer = Installer()
 
     if args.uninstall:
-        print(Fore.CYAN + "Removing VPN Manager"  + Fore.RESET)
+        print("\x1b[36m" + "Removing VPN Manager" + "\x1b[39m")
         installer.uninstall()
     else:
         downloader = Downloader()
 
-        print("Welcome to" + Fore.CYAN + " VPN Manager!" + Fore.RESET)
+        print("Welcome to" + "\x1b[36m" + " VPN Manager!" + "\x1b[39m")
         print()
-        print("This will download and install the latest version of" + Fore.CYAN + " vpnm," + Fore.RESET)
-        print("an alternative CLI client for" + Fore.CYAN + " VPN Manager." + Fore.RESET)
+        print(
+            "This will download and install the latest version of"
+            + "\x1b[36m"
+            + " vpnm,"
+            + "\x1b[39m"
+        )
+        print(
+            "an alternative CLI client for" + "\x1b[36m" + " VPN Manager." + "\x1b[39m"
+        )
         print()
         print("It will add the `vpnm` command to system's bin directory, located at:")
         print()
-        print(Fore.CYAN + downloader.bin_path.as_posix() + Fore.RESET)
+        print("\x1b[36m" + downloader.bin_path.as_posix() + "\x1b[39m")
         print()
-        print("You can uninstall at any time by executing this script with the --uninstall option,")
+        print(
+            "You can uninstall at any time by executing this script with the --uninstall option,"
+        )
         print("and these changes will be reverted.")
         print()
-        print(Fore.CYAN + "Retrieving dependencies..." + Fore.RESET)
+        print("\x1b[36m" + "Retrieving dependencies..." + "\x1b[39m")
         print()
-        
+
         downloader.process_urls()
-        
-        print(Fore.CYAN + "Installing VPN Manager..." + Fore.RESET)
+
+        print("\x1b[36m" + "Installing VPN Manager..." + "\x1b[39m")
         print()
 
         installer.install()
 
-        print(Fore.CYAN + "VPN Manager is installed now. Great!" + Fore.RESET)
+        print("\x1b[36m" + "VPN Manager is installed now. Great!" + "\x1b[39m")
         print()
         print("You can test that everything is set up by executing:")
         print("`vpnm --help`")
